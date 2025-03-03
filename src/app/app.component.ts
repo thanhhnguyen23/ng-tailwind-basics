d284ddb7374dc054063ba716f1cd6298765f2557
thanhhnguyen23 <thanh.h.nguyen23@gmail.com>
2025-03-02 20:22:22 -0500

    add daisyui components with utility-first philosophy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShellComponent } from './ui/components/daisy/shell/shell.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+    imports: [RouterOutlet, ShellComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ng-tailwind-basics';
+  title = 'Tailwind CSS';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { ShellComponent } from './ui/components/daisy/shell/shell.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: ShellComponent }];
